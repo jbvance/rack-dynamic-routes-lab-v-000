66306cd3c3353bfa6f3946ab907050698c5912f9
Jason Vance <jbvance@gmail.com>
2017-12-07 14:22:20 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,13 @@
+def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path.include?("/items/")
+      resp.write "You requested the songs"
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+ 
+    resp.finish
+  end
