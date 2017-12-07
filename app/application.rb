c5e7acc59cbc5e2d8536f1f56b1dd17bd6854829
Jason Vance <jbvance@gmail.com>
2017-12-07 14:30:52 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,14 @@
 class Application
+
+  @@items = []
+  
     def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.include?("/items/")
-      resp.write "You requested the songs"
+      item = req.path.split("/items/").last
+    
     else
       resp.write "Route not found"
       resp.status = 404
@@ -12,4 +16,14 @@ class Application
 
     resp.finish
   end
+
+  def item_price(item)
+    @@items.each do |i|
+      if item == i.name
+        return i.price
+      end
+    end
+    return nil
+  end
+  
 end
